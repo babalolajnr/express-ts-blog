de971fa2e62f1120f49c2b8a14346d2915b99256
Abdulqudduus <babalolajnr@gmail.com>
2022-06-10 11:21:23 +0100

    refactor: call controller methods directly from route

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -4,32 +4,20 @@ import { authenticate } from "../config/passport";
 import PostsController from "../controllers/posts";
 import { validate } from "../app";
 import createSchema from "../validation/posts";
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
 
 export class PostRoutes extends Router {
 
     constructor(app: express.Application) {
-        super(app, 'PostRoutes');
-        this.configureRoutes();
+        super(app, 'PostRoutes')
+        this.configureRoutes()
     }
 
     configureRoutes(): express.Application {
-        this.app.get('/posts', authenticate, (req, res) => {
-            PostsController.index(req, res);
-        })
+        this.app.get('/posts', authenticate, PostsController.index)
+        this.app.post('/posts/create', [authenticate, validate(createSchema)], PostsController.create)
+        this.app.patch('/posts/update/:id', [authenticate, validate(createSchema)], PostsController.update)
+        this.app.patch('/posts/delete/:id', [authenticate], PostsController.delete)
 
-        this.app.post('/posts/create', [authenticate, validate(createSchema)],
-            (req: express.Request, res: express.Response) => {
-                PostsController.create(req, res);
-            })
-
-        this.app.patch('/posts/update/:id', [authenticate, validate(createSchema)],
-            (req: express.Request, res: express.Response) => {
-                PostsController.update(req, res);
-            })
-
-
-        return this.app;
+        return this.app
     }
 }
