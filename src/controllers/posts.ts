b99819969ebcaa90775167fc914141b389aa90e3
Abdulqudduus <babalolajnr@gmail.com>
2022-06-10 11:22:40 +0100

    chore: add return types to methods

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -3,12 +3,12 @@ import express from "express"
 
 export default class PostsController {
 
-    public static async index(req: express.Request, res: express.Response) {
+    public static async index(req: express.Request, res: express.Response): Promise<express.Response> {
         const posts = await prisma.post.findMany()
         return res.json({ posts });
     }
 
-    public static async create(req: express.Request, res: express.Response) {
+    public static async create(req: express.Request, res: express.Response): Promise<express.Response> {
         const { title, content } = req.body;
 
         const post = await prisma.post.create({
@@ -20,10 +20,11 @@ export default class PostsController {
                 }
             }
         });
+
         return res.json({ post });
     }
 
-    public static async update(req: express.Request, res: express.Response) {
+    public static async update(req: express.Request, res: express.Response): Promise<express.Response> {
         const { id } = req.params;
         const { title, content } = req.body;
 
@@ -36,5 +37,15 @@ export default class PostsController {
         });
 
         return res.json({ message: "Post Updated", post });
+    }
+
+    public static async delete(req: express.Request, res: express.Response): Promise<express.Response> {
+        const { id } = req.params;
+
+        const post = await prisma.post.delete({
+            where: { id: parseInt(id) }
+        });
+
+        return res.json({ message: "Post Deleted" });
     }
 }
